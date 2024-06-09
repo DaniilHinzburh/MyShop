499d50cec1b92868d51a69708c1d105003089f0d
ilgan <2ilgan1601@gmail.com>
2024-06-09 22:40:24 +0300

    add Django models for Buyer, Operator, Manufacturer, Category, Subcategory, Product, Order and Check

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,67 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Buyer"
+        verbose_name_plural = "Buyers"
+
+
+class Operator(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    subcategory_list = models.ManyToManyField(Subcategory)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    description = models.TextField()
+    warranty_period = models.FloatField()
+
+
+class Order(models.Model):
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name="orders")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description_creation_date = models.DateTimeField(auto_now_add=True)
+    delivery_type = models.CharField(max_length=255)
+    delivery_address = models.TextField()
+    check = models.OneToOneField("Check", on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(max_length=255, choices=[
+        ('open',),
+        ('closed',),
+        ('sent',),
+        ('received',),
+        ('returned',),
+    ])
+
+
+class Check(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name="checks")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    warranty_period = models.FloatField()
+    price = models.FloatField()
