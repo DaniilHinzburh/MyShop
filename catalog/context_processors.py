7a00daeddff4fae529cf8dc91fd9babe59ecb498
ilgan <2ilgan1601@gmail.com>
2024-06-11 22:02:14 +0300

    update context_processors.py

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -1,13 +1,18 @@
 import random
 
-from .models import Category
+from .models import Category, Product
 
 
 def categories(request):
     return {'categories': Category.objects.all()}
 
 
-def random_categories(request):
-    categories = list(Category.objects.all())
-    random_categories = random.sample(categories, 8)
-    return {'random_categories': random_categories}
+def random_product(request):
+    try:
+        product = list(Product.objects.all())
+        random_product = random.sample(product, 8)
+        first_part = random_product[:4]
+        second_part = random_product[4:]
+        return {'first_part': first_part, 'second_part': second_part}
+    except Exception:
+        return {'first_part': [], 'second_part': []}
